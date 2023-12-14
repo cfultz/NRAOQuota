1fe17397baabcd2f7bddd7b7a0df0ea8243b1a9c
Caleb Fultz <caleb@fultz.dev>
2023-12-14 08:24:11 -0500

    updated script

--- quotafinder.sh
+++ quotafinder.sh
@@ -1,56 +1,60 @@
 #/bin/bash
 
+## Start script
+
 mainmenu () {
 
+clear
 echo
 echo $'    _   ______  ___   ____ '
 echo $'   / | / / __ \/   | / __ \ '
 echo $'  /  |/ / /_/ / /| |/ / / /'
 echo $' / /|  / _, _/ ___ / /_/ / '
 echo $'/_/ |_/_/ |_/_/  |_\____/ ' 
-echo        User Home Quota Script  
+echo $'  User Home Quota Script ' 
 echo
-echo        by cfultz@nrao.edu
+echo $'   by cfultz@nrao.edu      '
 echo
 echo "Press 1 to see your quota"
 echo "Press 2 to see 20 largest files"
 echo "Press 3 to find latest modified files (24h)"
-echo 
-echo 
+echo
+echo
 echo "Press x to exit the script"
+
 read -n 1 -s -r -p "Input Selection:" mainmenuinput
   if [ "$mainmenuinput" = "1" ]; then
             clear
-            echo 
-            echo 
+            echo
+            echo
             quota -s -u --show-mntpoint
             echo
             read -n 1 -s -r -p "Press any key to continue"
             echo
+            clear
             mainmenu
         elif [ "$mainmenuinput" = "2" ]; then
             clear
-            echo This will take a bit...
+            echo "This will take a bit..."
             echo "Even if it doesn't look like it's working, it is!"
             echo
-            echo "There may be 'Permision denied' errors if files/directories aren't owned by" $USER
-            find $HOME -type f -printf '%s %p\n' | sort -nr | head -20 | numfmt --to=iec
+            find $HOME -type f -printf '%s %p\n' -not -path '*/.*' 2>&1 | grep -v "Permission denied" | sort -nr | head -20 | numfmt --to=iec
             echo
             read -n 1 -s -r -p "Press any key to continue"
             echo
+            clear
             mainmenu
         elif [ "$mainmenuinput" = "3" ]; then
             clear
-            echo This will take a bit...
+            echo "This will take a bit..."
             echo "Even if it doesn't look like it's working, it is!"
             echo
-            echo If you dont see anything, nothing has changed!
-            find $HOME -mtime 1 -not -path '/.*' > $HOME/filesmodified24h.txt
-            cat $HOME/filesmodified24h.txt
+            find $HOME -mtime 1 -not -path '*/.*' 2>&1 | grep -v "Permission denied" | sort -nr |  numfmt --to=iec
+            echo "If you dont see anything, nothing has changed!"
             echo
-            echo 
             read -n 1 -s -r -p "Press any key to continue"
             echo
+            clear
             mainmenu
         elif [ "$mainmenuinput" = "x" ];then
             clear
